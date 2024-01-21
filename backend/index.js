2698d9f402f3c2d07d9ddb83912a97a99318b74b
Reza Karim <reza.karimkeystone@gmail.com>
2024-01-21 11:05:56 -0500

    Setup

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "f74547e8-6950-460e-90ab-9bbaa3caa2f4" } }
+    );
+
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
